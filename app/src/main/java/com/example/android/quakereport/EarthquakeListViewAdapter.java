848edaaecf4aa3a006653495f293f5ccc77aba52
Corey Murphy <c.murphy360@gmail.com>
2016-11-19 10:59:17 -0800

    Added Magnitude Circle Drawable Colors

--- app/src/main/java/com/example/android/quakereport/EarthquakeListViewAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeListViewAdapter.java
@@ -17,6 +17,8 @@ package com.example.android.quakereport;
 
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -74,8 +76,18 @@ public class EarthquakeListViewAdapter extends BaseAdapter {
         }
         Earthquake earthquake = getItem(position);
 
+
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude_text_view);
         magnitudeTextView.setText(formatMagnitude(earthquake.getMagnitude()));
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeTextView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(earthquake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
 
         TextView locationTextView = (TextView) listItemView.findViewById(R.id.location_text_view);
         locationTextView.setText(splitOffsetLocation(earthquake.getLocation()));
@@ -92,13 +104,39 @@ public class EarthquakeListViewAdapter extends BaseAdapter {
         return listItemView;
     }
 
-
-    public void add(Earthquake earthquake) {
-
-        quakeList.add(earthquake);
-
-
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeLevel = (int) magnitude;
+        int magnitudeColorResourceId;
+        Log.d(TAG, "getMagnitudeColor: " +magnitudeLevel);
+        if (magnitudeLevel > 10) {magnitudeLevel = 10;}
+        switch(magnitudeLevel){
+            case 1: magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2: magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3: magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4: magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5: magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6: magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7: magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8: magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9: magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            case 10: magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+            default: magnitudeColorResourceId = R.color.magnitude1;
+                break;
+        }
+        return ContextCompat.getColor(context, magnitudeColorResourceId);
     }
+
+
 
     /**
      * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
